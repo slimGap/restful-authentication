55c63822a7a4410b1ce03b9bfac5ce5e52049644
Sam Pierson <sam@sampierson.com>
2009-04-19 18:19:17 -0700

    Fixes for cucumber 3.0.
    Cucumber 3.0 now notices a matcher conflict:
    Multiple step definitions have the same Regexp: (Cucumber::Redundant)
    features/step_definitions/user_steps.rb:16:in `/^(.*) (.*) user named '(.*)'$/'
    features/step_definitions/user_steps.rb:29:in `/^there is no (.*) user named '(.*)'$/'
    I have no idea why cucumber 0.1.4 didn't notice that, but whatever.
    As a quick fix I changed '$an' to 'an' in the first one.

--- generators/authenticated/templates/features/step_definitions/user_steps.rb
+++ generators/authenticated/templates/features/step_definitions/user_steps.rb
@@ -13,7 +13,7 @@ Given "$an $user_type user with $attributes" do |_, user_type, attributes|
   create_user! user_type, attributes.to_hash_from_story
 end
 
-Given "$an $user_type user named '$name'" do |_, user_type, name|
+Given "an $user_type user named '$name'" do |user_type, name|
   create_user! user_type, named_user(name)
 end
 
